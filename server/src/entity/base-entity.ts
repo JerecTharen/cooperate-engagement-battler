545edc5da0dbdec0ff1df0d54f2b50266b819ee9
Drew <Drewfisher717@gmail.com>
2022-09-14 02:46:00 -0400

    comments for DTO/Entity

--- server/src/entity/base-entity.ts
+++ server/src/entity/base-entity.ts
@@ -1,5 +1,7 @@
 /**
- *class that defines the most basic fields of an entity class
+ * class that defines the most basic fields of an entity class.
+ * 
+ * Entity classes are directly mapped to our databases table schemas and should never be returned directly to the client. Use DTOs to send data from the server to the client to prevent breaking changes when updating database table schemas.
  *
  * All Entity classes must extend this class
  */
